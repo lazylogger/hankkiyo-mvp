d779de7d2792c0709bf57fe42d14914b30b79940
code4human <tena.kim10@gmail.com>
2020-07-07 20:49:30 +0900

    RDS와 연결

--- backend/mvp/config/settings.py
+++ backend/mvp/config/settings.py
@@ -29,7 +29,9 @@ SECRET_KEY = secrets['SECRET_KEY']
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [
+    'ec2-3-34-210-157.ap-northeast-2.compute.amazonaws.com',
+]
 
 
 # Application definition
