768be918cb311e58cdd184845c4b71a8c5cd26e6
code4human <tena.kim10@gmail.com>
2020-07-08 00:51:12 +0900

    api 코드 추가

--- backend/mvp/api/admin.py
+++ backend/mvp/api/admin.py
@@ -1,3 +1,17 @@
 from django.contrib import admin
+from . import models
 
-# Register your models here.
+
+@admin.register(models.Category)
+class CategoryAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(models.Store)
+class StoreAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(models.Menu)
+class MenuAdmin(admin.ModelAdmin):
+    pass
