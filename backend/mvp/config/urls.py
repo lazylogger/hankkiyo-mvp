b9ed8b2fbe3c4a0be01642392907bdc8f3f71225
code4human <tena.kim10@gmail.com>
2020-07-09 18:02:47 +0900

    이전 폴더 삭제

--- backend/mvp/config/urls.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from django.urls import path, include
-
-urlpatterns = [
-    path('admin/', admin.site.urls),
-
-    # my app
-    path('api/', include('api.urls'), name='api'),
-]
